29e0f516dfa9f57803b2ec95ea0c9661fb8e304d
MarcosOrtega180 <maortega@fctunca.edu.poy>
2022-06-14 07:23:59 -0400

    expose functionality using exports

--- Expose funcionality from a node file using exports/expose functionality using exports.js
+++ Expose funcionality from a node file using exports/expose functionality using exports.js
@@ -19,15 +19,69 @@
  * You can do so in 2 ways.
  *
  * The first is to assign an object to module.exports, which is an object provided out of the box by the module system,
- * and this will make your file exports jus that object.
+ * and this will make your file exports just that object.
  */
 
-const car = {
-  brand: 'Ford',
-  model: 'Fiesta',
-}
-
-module.exports = car;
+// const car = {
+//   brand: 'Ford',
+//   model: 'Fiesta',
+// }
+//
+// module.exports = car;
 // The variable car has been imported in the file index.js
 
+/**
+ * The second way is to add the exported object as a property of exports. This way allows you to export multiple
+ * objects, functions or data:
+ */
 
+// const car = {
+//   brand: 'Ford',
+//   model: 'Fiesta'
+// }
+//
+//
+// exports.car = car;
+
+// or directly
+
+// exports.car = {
+//   brand: 'Ford',
+//   model: 'Fiesta',
+// };
+/**
+ * And in the other file, you'll use it by referencing a prperty of your import:
+ *
+ * const items = require('./car');
+ *
+ * const { car } = items;
+ *
+ * Or you can use a destructuring assignment:
+ *
+ * const { car } = require('./car');
+ *
+ * What's the difference between module.exports and exports?
+ *
+ * The first exposes the object it points to. The latter exposes the properties of the object it points to.
+ *
+ * require will always return the object that module.exports poits to
+ */
+
+// car.js
+exports.car = {
+  brand: 'Ford',
+  model: 'Fiesta',
+};
+
+module.exports = {
+  brand: 'Tesla',
+  model: 'Model S',
+};
+
+// app.js (the other file)
+// const tesla = require('./car');
+// const ford = require('./car').car;
+//
+// console.log(tesla, ford);
+
+// This will print { brand: 'Tesla', model: 'Model S' } undefined since the require function's return value has been updated to the object that module.exports points to, so the property that exports added can't be accessed.
--- Expose funcionality from a node file using exports/index.js
+++ Expose funcionality from a node file using exports/index.js
@@ -1,3 +1,20 @@
-const car = require('./expose functionality using exports');
+// First method.
+// const car = require('./expose functionality using exports');
+//
+// console.log('imported car ', car);
 
-console.log('imported car ', car);
+
+// Second method
+
+// const items = require('./car');
+//
+// const { car } = items;
+
+//
+// const { car } = require('./car');
+
+
+const tesla = require('./car');
+const ford = require('./car').car;
+
+console.log(tesla, ford);
